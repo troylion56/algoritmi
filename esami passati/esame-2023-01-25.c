27cd8af2bc07783c066f5357c2e2cef7dc8e9a0e
troylion56 <bluphoenix56@gmail.com>
2023-01-25 18:48:13 +0100

    agigornamento

--- esami passati/esame-2023-01-25.c
+++ esami passati/esame-2023-01-25.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*esame 19-09-2022 quello che ha fatto bishoy*/			//corretto
-/*int verifica(oodo_albero* a, grafo* g)
-che accetti in imput un puntatore a alla radice di un albero binario di interi e un puntatore g ad un 
-grafo non orientato rappresentato tramite oggetti e riferimenti. La funzione restituisce 1 (cioè true) 
-se esiste un nodo con due figli nell'albero la cui profondità è uguale al numero di nodi della componente 
-connessa del grafo g che ha meno nodi altrimenti la funzione restituisce 0 (cioè false). Se uno 
-(o entrambi) tra grafo e albero è vuoto (cioè uguale a NULL) la funzione ritorna 0 (false).*/
+/*esame 2023-01-25 esame mio che probabilemnte devo rifare mannaggia al cazzo */	
+/*appna ho il testo scrivo qui*/	
 
 
 
@@ -56,97 +51,9 @@ typedef struct nodo_abero_struc {
 	int info;
 	struct nodo_abero_struc* left;
 	struct nodo_abero_struc* right;
+    struct nodo_abero_struc* parent;
 	char nome;
 }nodo_albero;
-
-
-/***************************************************************funzioni albero*****************************************************************/
-//----------------------new nodo-----------------------
-nodo_albero* newnodo (nodo_albero** a){
-	nodo_albero* new=malloc (sizeof(nodo_albero));
-	new->right=NULL;
-	new->left=NULL;
-	new->info=2;
-	new->nome='r';
-	*a=new;
-	printf("nodo creato\n");
- }
-//-------------------------------------------------------
-
-//-------------inserimento figlio sinistro---------------
-nodo_albero* inserimentofiglioleft (nodo_albero* a,int d,char nome){
-	if(a->left!=NULL)
-		printf("ERRORE\n");
-	else{
-		a->left=malloc(sizeof(nodo_albero));
-		a->left->info=d;
-		a->left->left=NULL;
-		a->left->right=NULL;
-		a->left->nome=nome;
-		printf("ho aggiunto il figlio left %d di %d \n",d,a->info);
-		return a->left;
-	}
-}
-//-------------------------------------------------------
-
-
-
-//-------------inserimento figlio destro---------------
-nodo_albero* inserimentofilgioright (nodo_albero* a,int d,char nome){
-	if(a->right!=NULL)
-		printf("ERRORE\n");
-	else{
-		a->right=malloc(sizeof(nodo_albero));
-		a->right->info=d;
-		a->right->left=NULL;
-		a->right->right=NULL;
-		a->right->nome=nome;
-		printf("ho aggiunto il figlio right %d di %d\n",d,a->info);
-		return a->right;
-	}
-}
-//-------------------------------------------------------
-
-
-
-//----------------costruisco l'albero--------------------
-
-nodo_albero* costrusci_albero () {
-	nodo_albero* n=NULL;						//inizializzo n
-	
-	newnodo (&n);
-	
-	nodo_albero* a=inserimentofiglioleft(n,1,'A');				//figlio left della n (a)
-	nodo_albero* b=inserimentofilgioright(n,2,'B');				//filgio right della n (b)
-	
-	nodo_albero* c=inserimentofiglioleft(a,3,'C');					//filgio left di a (c)
-	nodo_albero* d=inserimentofilgioright(a,4,'D');					//filgio right di a (d)
-	
-	nodo_albero* e=inserimentofiglioleft(b,5,'E');					//filgio left di b (e)
-	
-	nodo_albero* f=inserimentofiglioleft(d,6,'F');					//figlio left di b (f)
-	nodo_albero* g=inserimentofilgioright(d,7,'G');					//figlio right di b (g)
-	
-	nodo_albero* h=inserimentofiglioleft(e,8,'H');					//figlio left di e (h)
-	nodo_albero* i=inserimentofilgioright(e,9,'I');					//figlio right di e (i)
-	
-	nodo_albero* l=inserimentofiglioleft(g,10,'L');					//filgio left di g (l)
-	
-	return n;
-	
-} 
-
-/*           n
- *          / \ 
- *         a   b
- *        /\    / 
- *       c d    e
- *         /\   /\
- *         f g h  i
- *           /
- *           l            */
-//-------------------------------------------------------
-/****************************************************************************************************************************************************/
 
 
 
@@ -267,26 +174,23 @@ grafo* costruisci_grafo() {
 //-------------------------------------------------------
 /****************************************************************************************************************************************************/
 
-int is_foglia (nodo_albero* a) {
-	if(a==NULL)
-		return 0;
-	if( (a->left==NULL) && (a->right==NULL))
-		return 1;
-	return 0;
+void inserimento(nodo_albero* a,int val,nodo_albero* p) {
+    if(a==NULL) {
+          a=malloc(sizeof(nodo_albero));
+          a->info=val;
+          a->left=NULL;
+          a->right=NULL;
+          a->parent=p;
+    }
+    else {
+        if(a->info>val) {
+            inserimento(a->left,val,a);
+        }
+        else {
+            inserimento(a->right,val,a);
+        }
+    }
 }
-
-
-//faccio il confrono e la verica in questa funzione chiamo una funzione d'appoggio che mi verifica se sono una foglia
-int verifica_figli_visita (nodo_albero* a,int alt, int dim_gra) {	//dim_gra è la dimensione del sottografo piu grande
-	if(a==NULL)
-		return 0;
-	int l= verifica_figli_visita(a->left,alt+1,dim_gra);
-	int r= verifica_figli_visita(a->right,alt+1,dim_gra);
-	if(alt==dim_gra && ((is_foglia(a->left)) && (is_foglia(a->right)) ))
-		return 1;
-	return l || r;
-}
-
 
 
 void dfs(nodo* g,int c) {
@@ -302,11 +206,9 @@ void dfs(nodo* g,int c) {
 	}
 }
 
-int verifica (grafo* g, nodo_albero* a, int h) {
-	if(g==NULL && a==NULL)
-		return 0;
-	if(g==NULL || a==NULL)
-		return 0;
+nodo_albero* abr (grafo* g) {
+	if(g==NULL)
+        return 0;
 	elem_nodi* scorri1=g->nodi;
 	int colore=0;
 	while(scorri1!=NULL) {
@@ -328,31 +230,20 @@ int verifica (grafo* g, nodo_albero* a, int h) {
 		temp2=temp2->next;
 	}
 	//-------------------------
-	
-	int min=vett[1];
-	for(int i=1;i<=colore;i++) {
-		printf("vett [%d]=%d\n",i,vett[i]);
-		if(vett[i]<min)
-			min=vett[i];
-	}
-	printf("la componente minore ha %d nodi\n",min);
-	
-	if(verifica_figli_visita(a,h,min))
-		return 1;
-	return 0;
+    nodo_albero* root=malloc(sizeof(nodo_albero));
+    root->info=vett[1];
+    for (int i=2;i<colore;i++) {
+        inserimento(root,vett[i],NULL);
+    }
+
 }
-
-
-
 
 
 //---------------------main------------------------------
 int main(int argc, char **argv){
 	
 	grafo* grap=costruisci_grafo();
-	nodo_albero* alb=costrusci_albero();
-	
-	printf("%d",verifica(grap,alb,0));
+	printf("%d",abr(grap));
 	
 }
 //-------------------------------------------------------
